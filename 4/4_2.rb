67fa15412c574305615afd5068ae0675f9e2ebe7
Aleksandra <solveug@rambler.ru>
2021-05-25 13:47:49 +0300

    lesson 4

---
+++ 4/4_2.rb
@@ -0,0 +1,6 @@
+print 'Введите первое число: '
+x = gets.to_f
+print 'Введите второе число: '
+y = gets.to_f
+average = (x + y) / 2
+print "Среднее арифметическое: #{average}\n"
---
+++ 4/4_4.rb
@@ -0,0 +1,14 @@
+turn = rand (1..2)
+rebro = rand (1..10)
+
+if rebro == 10
+  puts "На ребро"
+  exit
+else
+
+if turn == 1
+  puts "Выпал орел"
+else
+  puts "Выпала решка"
+  end
+end
