b36d3117dc7765a512098f3e865c36298069bf47
Aleksandra <solveug@rambler.ru>
2021-05-25 14:03:48 +0300

    update 4_4.rb

--- 4/4_4.rb
+++ 4/4_4.rb
@@ -1,3 +1,15 @@
+# вариант решения с массивом
+arr = [0, 0, 0, 0, 1, 1, 1, 1, 2]
+coin = arr.sample
+if coin == 2
+  puts "на ребро"
+elsif coin == 1
+  puts "выпал орел"
+else
+  puts "выпала решка"
+end
+
+# вариант решения с диапазоном
 turn = rand (1..2)
 rebro = rand (1..10)
 
