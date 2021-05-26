79702f833cd446ab89a4c9c9c3912c6bc070a07d
Aleksandra <solveug@rambler.ru>
2021-05-26 17:59:54 +0300

    lesson 8

---
+++ 8/8_2.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+puts 'Введите строку, которую надо проверить на «палиндромность»:'
+original_string = gets.downcase.delete('^a-zа-яё')
+pp original_string
+reverse_string = original_string.reverse
+if reverse_string == original_string
+  puts 'Да, это палиндром'
+else
+  puts 'Нет, это не палиндром'
+end
