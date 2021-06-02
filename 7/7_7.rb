4c5f0bda90093d1613a401791311bc814fc45523
Aleksandra <solveug@rambler.ru>
2021-06-02 14:04:16 +0300

    another solution

--- 7/7_7.rb
+++ 7/7_7.rb
@@ -4,3 +4,7 @@ while crypto_str.length <= 8
   crypto_str += massiv.sample.to_s
 end
 puts crypto_str
+
+# второй вариант решения
+massiv = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+8.times {  print massiv.sample }
