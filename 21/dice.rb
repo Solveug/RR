e8457e561b95801b94a38b30e19aabb34eb51d0e
Aleksandra <solveug@rambler.ru>
2021-07-07 13:22:43 +0300

    add dice.rb (21_4) to lesson 21

--- 21/dice.rb
+++ 21/dice.rb
@@ -1,9 +1,17 @@
-print "How many dice?: "
+# frozen_string_literal: true
+
+print 'How many dice?: '
 number = gets.to_i
+total = 0
+
 number.times do
   10.times do
     print "#{rand(7)}\r"
     sleep 0.1
   end
-  puts rand(6) + 1
+  rand = rand(1..6)
+  puts rand
+  total += rand
 end
+
+puts "Sum of dice: #{total}"
